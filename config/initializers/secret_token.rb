8b734e21d212d210acace5fc3a27b37dc8e3b093
Hassan Akram <hassan.akram3282@gmail.com>
2015-08-17 10:56:49 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VirtualShoppingMall::Application.config.secret_token = '57364877353e3db780b6f9f212db576b9b73259950d612f18f4411e52bb94db803f97330b78e900a5413476e6bb25eb4c540791af947586a52ee69237b7fb588'
