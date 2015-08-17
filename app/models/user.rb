dd40eb4ee5af364a82121a7228e61daf5ab4e08c
Hassan Akram <hassan.akram3282@gmail.com>
2015-08-17 17:47:21 +0500

    User: added image to user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :image_attributes
+
+  has_one :image, as: :imageable, dependent: :destroy
+
+  accepts_nested_attributes_for :image
+
+  def fetch_image
+    self.image.present? ? self.image : self.build_image
+  end
+
 end
