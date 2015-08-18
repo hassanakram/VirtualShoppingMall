99d211b06fa0ed786739ad155a43d2ce2d7ecebc
Hassan Akram <hassan.akram3282@gmail.com>
2015-08-18 10:23:41 +0500

    Product: added pagination to index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   respond_to :html
 
   def index
-    @products = Product.all
+    @products = Product.page(params[:page]).per(5)
     respond_with(@products)
   end
 
