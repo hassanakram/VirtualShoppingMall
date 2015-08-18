f1d53b01e7b2c437ecf5cbc5c040a640189f4f90
Hassan Akram <hassan.akram3282@gmail.com>
2015-08-18 11:52:24 +0500

    Image: remove uniqueness validation

--- app/models/image.rb
+++ app/models/image.rb
@@ -7,6 +7,5 @@ class Image < ActiveRecord::Base
   has_attached_file :photo, styles: { medium: "500x500>", thumb: "100x100>" }
 
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
-  validates_uniqueness_of :imageable_id, scope: :imageable_type
 
 end
