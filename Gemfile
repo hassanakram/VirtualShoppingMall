21d01027b4a14c60e926f4dbc6f2db0675836b76
Hassan Akram <hassan.akram3282@gmail.com>
2015-08-18 12:19:21 +0500

    Product: added images to product

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'mysql2'
 gem 'devise'
 gem "paperclip"
 gem 'kaminari'
+gem "nested_form"
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
-  attr_accessible :body, :price, :title
+
+  attr_accessible :body, :price, :title, :images_attributes
+
+  has_many :images, as: :imageable, dependent: :destroy
+
+  accepts_nested_attributes_for :images, :allow_destroy => true
+
 end
