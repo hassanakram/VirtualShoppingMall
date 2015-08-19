2919e8c3189230a113c1013521aa13453904852a
Hassan Akram <hassan.akram3282@gmail.com>
2015-08-19 12:55:49 +0500

    Show user's products on show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @products = @user.products.ordered.page(params[:page])
   end
 
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,7 @@
 module ProductsHelper
+
+  def authorised_user(user)
+    user_signed_in? && current_user.id == user.id
+  end
+
 end
