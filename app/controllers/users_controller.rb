2786f79eaa5c86554a985231bb0c0eb5aa5734b4
Hassan Akram <hassan.akram3282@gmail.com>
2015-08-19 16:00:39 +0500

    show user reviews on show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
-    @products = @user.products.ordered.page(params[:page])
+    @user = User.includes(products: [:reviews, :images]).includes(reviews: [:product]).find(params[:id])
+    @products = @user.products.ordered.page(params[:product_page])
+    @reviews = @user.reviews.ordered.page(params[:review_page])
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,13 @@
 class Review < ActiveRecord::Base
 
+  PER_PAGE_SIZE = 20
+  paginates_per PER_PAGE_SIZE
+
   attr_accessible :review
 
   belongs_to :product
   belongs_to :user
+
+  scope :ordered, order('created_at DESC')
 
 end
