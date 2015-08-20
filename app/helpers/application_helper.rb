6afcc22fca6edf9ec0b2cf879f13988cc79b64e3
Hassan Akram <hassan.akram3282@gmail.com>
2015-08-20 17:27:11 +0500

    User: dashboard and show page added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def authorised_owner(user_id)
     user_signed_in? && current_user.id == user_id
   end
+
+  def render_title(title)
+    title.blank? ? title.titleize : ""
+  end
 end
