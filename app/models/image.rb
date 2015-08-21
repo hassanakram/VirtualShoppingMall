43356ccc35ddcec5084204edd093e1795ba7341f
Hassan Akram <hassan.akram3282@gmail.com>
2015-08-21 12:07:30 +0500

    Product: added validations

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,6 +6,6 @@ class Image < ActiveRecord::Base
 
   has_attached_file :photo, styles: { medium: "500x500>", thumb: "270x180!" }
 
-  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/, presence: true
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,11 +5,14 @@ class Product < ActiveRecord::Base
 
   attr_accessible :body, :price, :title, :images_attributes
 
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :price, presence: true
+
   has_many :images, as: :imageable, dependent: :destroy
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   belongs_to :user
 
-  accepts_nested_attributes_for :images, :allow_destroy => true
+  accepts_nested_attributes_for :images, allow_destroy: true
 
   scope :ordered, order('created_at DESC')
 
